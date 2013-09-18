3f61e74b5e2306f220860cd670c68983ba097c68
harry_jumeau <hjumeau.prestataire@vsc-technologies.com>
2013-09-18 13:56:32 +0200

    first commit on github

---
+++ js/fix-slide.js
@@ -0,0 +1,39 @@
+function handleBodyKeyDown(event) {
+  if (/^(input|textarea)$/i.test(event.target.nodeName) || event.target.isContentEditable) {
+    return;
+  }
+  switch (event.keyCode) {
+    case 39: // right arrow
+    case 13: // Enter
+    case 32: // space
+    case 34: // PgDn
+      nextSlide();
+      event.preventDefault();
+      break;
+
+    case 37: // left arrow
+    case 8: // Backspace
+    case 33: // PgUp
+      prevSlide();
+      event.preventDefault();
+      break;
+
+    case 40: // down arrow
+      if (isChromeVoxActive()) {
+        speakNextItem();
+      } else {
+        nextSlide();
+      }
+      event.preventDefault();
+      break;
+
+    case 38: // up arrow
+      if (isChromeVoxActive()) {
+        speakPrevItem();
+      } else {
+        prevSlide();
+      }
+      event.preventDefault();
+      break;
+  }
+};
