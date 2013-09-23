ff39adf5ba6dbb2534b3ee37d3a464d55880e24e
harry_jumeau <hjumeau.prestataire@vsc-technologies.com>
2013-09-23 12:04:57 +0200

    Add webDateBases file

---
+++ js/webDataBases.js
@@ -0,0 +1,85 @@
+(function(){
+
+    var webdb = {};
+    webdb.db = null;
+    var addTodoButton = document.getElementById("addTodo");
+
+    webdb.open = function() {
+      var dbSize = 5 * 1024 * 1024; // 5MB
+      webdb.db = openDatabase("Todo", "1.0", "Todo manager", dbSize);
+    }
+
+    webdb.createTable = function() {
+      var db = webdb.db;
+      db.transaction(function(tx) {
+        tx.executeSql("CREATE TABLE IF NOT EXISTS todo(ID INTEGER PRIMARY KEY ASC, todo TEXT, added_on DATETIME)", []);
+      });
+    }
+
+    webdb.addTodo = function(todoText) {
+      var db = webdb.db;
+      db.transaction(function(tx){
+        var addedOn = new Date();
+        tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)",
+            [todoText, addedOn],
+            webdb.onSuccess,
+            webdb.onError);
+       });
+    }
+
+    webdb.onError = function(tx, e) {
+      alert("There has been an error: " + e.message);
+    }
+
+    webdb.onSuccess = function(tx, r) {
+      // re-render the data.
+      webdb.getAllTodoItems(loadTodoItems);
+    }
+
+
+    webdb.getAllTodoItems = function(renderFunc) {
+      var db = webdb.db;
+      db.transaction(function(tx) {
+        tx.executeSql("SELECT * FROM todo", [], renderFunc,
+            webdb.onError);
+      });
+    }
+
+    webdb.deleteTodo = function(id) {
+      var db = webdb.db;
+      db.transaction(function(tx){
+        tx.executeSql("DELETE FROM todo WHERE ID=?", [id],
+            webdb.onSuccess,
+            webdb.onError);
+        });
+    }
+
+    function loadTodoItems(tx, rs) {
+      var rowOutput = "";
+      var todoItems = document.getElementById("todoItems");
+      for (var i=0; i < rs.rows.length; i++) {
+        todoItems.innerHTML = renderTodo(rs.rows.item(i));
+        var row = document.getElementById("webdb_"+rs.rows.item(i).ID)
+        row.onclick = webdb.deleteTodo(rs.rows.item(i).ID);
+      }
+    }
+
+    function renderTodo(row) {
+      return "<li>" + row.todo  + " [<div id=" + "webdb_" + row.ID + ">Delete</div>]</li>";
+    }
+
+    function init() {
+      webdb.open();
+      webdb.createTable();
+      webdb.getAllTodoItems(loadTodoItems);
+      addTodoButton.onclick = addTodo;
+    }
+
+    function addTodo() {
+      var todo = document.getElementById("todo");
+      webdb.addTodo(todo.value);
+      todo.value = "";
+    }
+
+    window.addEventListener("load", init, false);
+})();
